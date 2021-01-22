03299c3ccf9d370039c1d0b2e665e877547e68f9
Polynomialdivision <vincent@systemli.org>
2021-01-22 18:59:56 +0100

    update

--- src/xdp-srv6-remover.c
+++ src/xdp-srv6-remover.c
@@ -2,10 +2,30 @@
 
 #include <arpa/inet.h>
 
-#include "uxdp.h"
 #include "common.h"
+#include "uxdp.h"
+
+static bool cidr_print6(struct cidr *a) {
+  char *p;
+
+  if (!a)
+    return NULL;
+
+  if (!(p = (char *)inet_ntop(AF_INET6, &a->addr.v6, a->buf.v6,
+                              sizeof(a->buf.v6))))
+    return false;
+
+  printf("%s", p);
+
+  if (a->prefix < 128)
+    printf("/%u", a->prefix);
+
+  return true;
+}
 
 static struct cidr *cidr_parse6(const char *s) {
+  /* copied from owipcalc.c */
+
   char *p = NULL, *r;
   struct cidr *addr = malloc(sizeof(struct cidr));
 
@@ -39,52 +59,54 @@ err:
   return NULL;
 }
 
-int
-main(int argc, char **argv)
-{
-	struct cidr *cidr;
-	struct xdp_map xdp_map = {
-		.prog = "xdp_srv6_func",
-		.map = "prefix_map",
-		.map_want = {
-			.key_size = sizeof(__u32),
-			.value_size = sizeof(struct cidr),
-			.max_entries = 1,
-		},
-	};
-	int ch;
-	
-	while ((ch = getopt(argc, argv, "d:f:p:")) != -1) {
-		switch (ch) {
-		case 'd':
-			xdp_map.net = optarg;
-			break;
-		case 'p':
-			// parse prefix
-			cidr = cidr_parse6(optarg);
-			break;
-		default:
-			fprintf(stderr, "Invalid argument\n");
-			exit(-1);
-		}
-	}
-	if (!xdp_map.net) {
-		fprintf(stderr, "invalid arguments\n");
-		return -1;
-	}
+int main(int argc, char **argv) {
+  struct cidr *prefix = NULL;
 
-	if (map_lookup(&xdp_map)) {
-		fprintf(stderr, "failed to xdp_map map\n");
-		return -1;
-	}
+  struct xdp_map xdp_map = {
+      .prog = "xdp_srv6_func",
+      .map = "prefix_map",
+      .map_want =
+          {
+              .key_size = sizeof(__u32),
+              .value_size = sizeof(struct cidr),
+              .max_entries = 1,
+          },
+  };
+  int ch;
 
-	int key = 0;
-	if (bpf_map_update_elem(xdp_map.map_fd, &key, &cidr, 0) < 0) {
-		fprintf(stderr,
-			"WARN: Failed to update bpf map file: err(%d):%s\n",
-			errno, strerror(errno));
-		return -1;
-	}
+  while ((ch = getopt(argc, argv, "d:f:p:")) != -1) {
+    switch (ch) {
+    case 'd':
+      xdp_map.net = optarg;
+      break;
+    case 'p':
+      // parse prefix
+      prefix = cidr_parse6(optarg);
+      break;
+    default:
+      fprintf(stderr, "Invalid argument\n");
+      exit(-1);
+    }
+  }
+  if (!xdp_map.net) {
+    fprintf(stderr, "invalid arguments\n");
+    return -1;
+  }
 
-	return 0;
+  if (map_lookup(&xdp_map)) {
+    fprintf(stderr, "failed to xdp_map map\n");
+    return -1;
+  }
+
+  printf("Updating Map with: ");
+  cidr_print6(prefix);
+
+  int key = 0;
+  if (bpf_map_update_elem(xdp_map.map_fd, &key, prefix, 0) < 0) {
+    fprintf(stderr, "WARN: Failed to update bpf map file: err(%d):%s\n", errno,
+            strerror(errno));
+    return -1;
+  }
+
+  return 0;
 }
